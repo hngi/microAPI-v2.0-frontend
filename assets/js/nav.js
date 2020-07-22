db79865fce9e28d644b9bf8a00fe6655eb93a4cd
Kadet <techkadet@gmail.com>
2020-07-22 12:22:34 +0100

    completed desktop/mobile site layout

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,8 +8,9 @@ document.querySelector('.hamburger').addEventListener('click', () => {
 
 document.querySelector('.mobile__menu').addEventListener('click', (e) => {
   let nav = document.querySelector('.mobile__menu nav');
+  let greyArea = document.querySelector('.mobile__menu');
 
-  if (e.target !== nav) {
+  if (e.target == greyArea) {
     nav.classList.add('nav--close');
     document.querySelector('.mobile__menu').classList.add('mobile__menu--close');
 
